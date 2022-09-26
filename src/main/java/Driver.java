dce0f21b318348980a4cf00094c2be8e39da4498
YahyaAzzam <96234204+YahyaAzzam@users.noreply.github.com>
2022-09-27 00:43:59 +0200

    initialize

---
+++ src/main/java/Driver.java
@@ -0,0 +1,38 @@
+package main.java.com.example;
+
+import com.google.common.base.Strings;
+
+/**
+ * This is java doc.
+ * @author maboubakr
+ *
+ */
+public class Driver implements IDriver {
+
+    @Override
+    public void createDatabase(String databaseSchemaPath) {
+
+    }
+
+    @Override
+    public void setRow(String databaseName, String tableName, String value) {
+
+    }
+
+    @Override
+    public void getRow(String database, String table, String value) {
+
+    }
+
+    @Override
+    public void deleteRow(String databaseName, String tableName, String value) {
+
+    }
+
+    /**
+     * This is a java doc.
+     */
+    public static void main(final String[] args) {
+
+    }
+}
---
+++ src/test/java/DriverTest.java
@@ -0,0 +1,16 @@
+package test.java.com.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import main.java.com.example.Driver;
+
+
+public class DriverTest {
+	@Test
+	void test() {
+
+	}
+}
