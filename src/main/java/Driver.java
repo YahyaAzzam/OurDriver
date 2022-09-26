715d3ea996c7d9fcf9cb86e422e2b8ac8237a0cb
YahyaAzzam <96234204+YahyaAzzam@users.noreply.github.com>
2022-09-27 01:18:17 +0200

    fix

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -7,8 +7,28 @@ package main.java;
  * @author yazzam
  *
  */
-public class Driver {
+public class Driver implements IDriver {
 
+
+    @Override
+    public void createDatabase(final String databaseSchemaPath) {
+
+    }
+
+    @Override
+    public void setRow(final String databaseName, final String tableName, final String value) {
+
+    }
+
+    @Override
+    public void getRow(final String database, final String table, final String value) {
+
+    }
+
+    @Override
+    public void deleteRow(final String databaseName, final String tableName, final String value) {
+
+    }
 
     /**
      * This is a java doc.
