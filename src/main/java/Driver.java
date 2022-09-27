a8a0be7a4e1113f6673c31ab747935c431dd4d64
YahyaAzzam <96234204+YahyaAzzam@users.noreply.github.com>
2022-09-27 05:10:09 +0200

    Interface (#1)
    Interface added

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -2,13 +2,39 @@ package main.java;
 
 //import com.google.common.base.Strings;
 
+import org.json.JSONArray;
+
 /**
  * This is java doc.
  * @author yazzam
  *
  */
-public class Driver {
+public final class Driver implements IDriver {
 
+    @Override
+    public void createDatabase(final String databaseSchemaPath) {
+
+    }
+
+    @Override
+    public void setRow(final String databaseName, final String tableName, final String value) {
+
+    }
+
+    @Override
+    public JSONArray getRow(final String databaseName, final String tableName, final String value) {
+        return null;
+    }
+
+    @Override
+    public boolean deleteRow(final String databaseName, final String tableName, final String value) {
+        return true;
+    }
+
+    @Override
+    public boolean clearDatabase(final String databaseName) {
+        return true;
+    }
 
     /**
      * This is a java doc.
