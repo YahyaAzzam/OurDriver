43f19f52382bab5f9516fa7c2a25a18b9b2e221a
YahyaAzzam <96234204+YahyaAzzam@users.noreply.github.com>
2022-10-01 23:51:57 +0200

    Add files via upload

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -2,45 +2,89 @@ package main.java;
 
 //import com.google.common.base.Strings;
 
+import main.java.Exceptions.NoParameterException;
+import main.java.Exceptions.OverwriteException;
+import main.java.Exceptions.WrongParameterException;
 import org.json.JSONArray;
+import org.json.JSONObject;
 
-/**
- * This is java doc.
- * @author yazzam
- *
- */
+
 public final class Driver implements IDriver {
 
-    @Override
-    public void createDatabase(final String databaseSchemaPath) {
+    private final Executor executor;
 
+    /**
+     * @param executor
+     *      The Executor object
+     */
+    public Driver(final Executor executor) {
+        this.executor = executor;
     }
 
     @Override
-    public void setRow(final String databaseName, final String tableName, final String value) {
-
+    public void createDatabase(final String databaseSchemaPath) throws Exception {
+        String command = String.format(" -c create -sc %s", databaseSchemaPath);
+        JSONObject api = executor.execute(command);
+        check(api);
     }
 
     @Override
-    public JSONArray getRow(final String databaseName, final String tableName, final String value) {
-        return null;
-    }
-
-    @Override
-    public boolean deleteRow(final String databaseName, final String tableName, final String value) {
-        return true;
-    }
-
-    @Override
-    public boolean clearDatabase(final String databaseName) {
-        return true;
+    public void setRow(final String databaseName, final String tableName, final String value) throws Exception {
+        String command = String.format(" -c set -db %s -t %s -q %s", databaseName, tableName, value);
+        JSONObject api = executor.execute(command);
+        check(api);
     }
 
     /**
-     * This is a java doc.
-     * @param args
+     * @param databaseName
+     * @param tableName
+     * @return
+     *      Array of json objects of all the rows of the table
      */
-    public void main(final String[] args) {
+    public JSONArray getRow(final String databaseName, final String tableName) throws Exception {
+        String command = String.format(" -c get -db %s -t %s -q ", databaseName, tableName);
+        JSONObject api = executor.execute(command);
+        check(api);
+        return (JSONArray) api.get("result");
+    }
 
+    @Override
+    public JSONArray getRow(final String databaseName, final String tableName, final String value) throws Exception {
+        String command = String.format(" -c get -db %s -t %s -q %s", databaseName, tableName, value);
+        JSONObject api = executor.execute(command);
+        check(api);
+        return (JSONArray) api.get("result");
+    }
+
+    @Override
+    public void deleteRow(final String databaseName, final String tableName, final String value) throws Exception {
+        String command = String.format(" -c delete -db %s -t %s -q %s", databaseName, tableName, value);
+        JSONObject api = executor.execute(command);
+        check(api);
+    }
+
+    @Override
+    public void clearDatabase(final String databaseName) throws Exception {
+        String command = String.format(" -c clear -db %s", databaseName);
+        JSONObject api = executor.execute(command);
+        check(api);
+    }
+
+    /**
+     * @param api
+     *      Api which the function will receive
+     */
+    private static void check(final JSONObject api) throws Exception {
+        if (api == null) {
+            throw new WrongParameterException("Check the parameters");
+        }
+        String status = (String) api.get("status");
+        if (status.equals(Errors.NoParameterError.toString())) {
+            throw new NoParameterException((String) api.get("message"));
+        } else if (status.equals(Errors.WrongParameterError.toString())) {
+            throw new WrongParameterException((String) api.get("message"));
+        } else if (status.equals(Errors.OverwriteError.toString())) {
+            throw new OverwriteException((String) api.get("message"));
+        }
     }
 }
